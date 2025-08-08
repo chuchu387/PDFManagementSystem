61da28419fbc3bdf8b292ed5e9293697f685bce1
chuchu387 <heyitsmeshubhamofficial69@gmail.com>
2025-08-08 15:29:14 +0545

    make changes on service

--- src/main/java/com/example/pdfmanagement/service/CustomUserDetails.java
+++ src/main/java/com/example/pdfmanagement/service/CustomUserDetails.java
@@ -1,7 +1,6 @@
 package com.example.pdfmanagement.service;
 
 import com.example.pdfmanagement.model.AppUser;
-import com.example.pdfmanagement.model.Role;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
